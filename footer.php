08608e155e077473b4dc140d61d7508e2a92c05f
Proshenjit Karmakar <proshnjit@gmail.com>
2022-06-15 12:20:15 +0600

    CRUD

---
+++ footer.php
@@ -0,0 +1,21 @@
+	
+    
+    <!--|---------------------------|
+       |    All Js File Here       |
+       |---------------------------| -->
+	<!-- jQuery 3.5.1 Verson File -->
+	<script type="text/javascript" src="js/jquery-3.6.0.js"></script>
+	<!-- js for popper -->
+	<script type="text/javascript" src="js/popper.js"></script>
+	<!-- js fo bootstrap -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!-- js for fontawesome -->
+	<script type="text/javascript" src="js/all.min.js"></script>
+    <!-- jQuery table cnd  -->
+    <script src="//cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="js/jQtable.js"></script>
+
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!--  |---------------------------|
+          |      Meta Tags Here       |
+          |---------------------------| -->
+	<!--turn off IE compatibility mode -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<!-- Charecter set -->
+	<meta charset="utf-8">
+	<!-- View Port -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1" />
+
+	<!-- |---------------------------|
+       	|    Website Onpage SEO     |
+        |---------------------------| -->
+
+	<!-- This is title -->
+	<title>Insert Delete Update</title>
+	<!-- Keyword -->
+	<meta name="Keyword" content="Insert Delete Update ">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+
+	<!-- |---------------------------|
+        |    All Css File Here      |
+        |---------------------------| -->
+
+	<!-- css for bootstrap -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<!-- CSS for FontAwesome -->
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+
+	<!-- My css file -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- jQuery Table cnd  -->
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+
+
+</head>
+<body>
